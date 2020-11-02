6ff6b30e8a3867485a2894e53566b8eca5dad32f
marcus.billman <54668482+marcusbillman@users.noreply.github.com>
2020-11-02 11:08:00 +0100

    Create User class with basic skeleton

---
+++ src/User.java
@@ -0,0 +1,20 @@
+public class User {
+    private String name;
+    private String id;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
