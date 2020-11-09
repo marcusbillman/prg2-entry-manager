97a9d62186ea03088cfff9c9ef66d2299c163f4e
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2020-11-09 19:17:37 +0100

    Add methods to View

--- src/View.java
+++ src/View.java
@@ -1,17 +1,39 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class View {
+    private JFrame frame;
     private JPanel panel;
     private JButton createEntryButton;
-    private JTextArea textArea1;
-    private JComboBox comboBox1;
-    private JTable table1;
+    private JTextArea newEntryTextArea;
+    private JComboBox authorComboBox;
+    private JTable entriesTable;
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Entry Manager");
-        frame.setContentPane(new View().panel);
+    View(String title) {
+        this.frame = new JFrame(title);
+        frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public String getNewEntryContent() {
+        return newEntryTextArea.getText();
+    }
+
+    public String getAuthorName() {
+        return (String) authorComboBox.getSelectedItem();
+    }
+
+    void addCreateEntryListener(ActionListener actionListener) {
+        createEntryButton.addActionListener(actionListener);
+    }
+
+    // TODO: populateAuthorComboBox()
+
+    // TODO: populateEntriesTable()
+
+    void displayErrorMessage(String message, String title) {
+        JOptionPane.showMessageDialog(frame, message, title, JOptionPane.ERROR_MESSAGE);
     }
 }
