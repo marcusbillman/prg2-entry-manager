3b50e9fca037b7023c96a822e1e15661767c5c9a
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2020-11-09 19:20:08 +0100

    Create Controller class

---
+++ src/Controller.java
@@ -0,0 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller {
+    private View view;
+    private EntryManager entryManager;
+
+    public Controller(EntryManager entryManager, View view) {
+        this.entryManager = entryManager;
+        this.view = view;
+    }
+
+    private class CreateEntryListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            try {
+                // TODO: Create the entry
+            }
+            catch (Exception ex) {
+                System.out.println(ex);
+                view.displayErrorMessage("Oops! Something went wrong.", "Error");
+            }
+        }
+    }
+}
