2d53e08a19010152c50c2d0dde966f1b420860a9
marcus.billman <54668482+marcusbillman@users.noreply.github.com>
2020-11-16 11:13:29 +0100

    Create FileIO class with basic implementation

---
+++ src/FileIO.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class FileIO {
+    public void save(EntryManager entryManager, String fileName) throws IOException {
+        ObjectOutputStream out = null;
+
+        try {
+            out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)));
+            out.writeObject(entryManager);
+        } finally {
+            out.close();
+        }
+    }
+
+    public EntryManager load(String fileName) throws IOException, ClassNotFoundException {
+        EntryManager entryManager;
+
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)));
+            entryManager = (EntryManager) in.readObject();
+        } finally {
+            in.close();
+        }
+
+        return entryManager;
+    }
+}
