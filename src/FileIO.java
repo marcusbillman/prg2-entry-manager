74db3e1e756851f6565cac6337b85d16571d3c8b
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2020-12-15 13:09:59 +0100

    FileIO, Main: fix warnings

--- src/FileIO.java
+++ src/FileIO.java
@@ -2,25 +2,16 @@ import java.io.*;
 
 public class FileIO {
     public static void save(EntryManager entryManager, String fileName) throws IOException {
-        ObjectOutputStream out = null;
-
-        try {
-            out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)));
+        try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))) {
             out.writeObject(entryManager);
-        } finally {
-            out.close();
         }
     }
 
     public static EntryManager load(String fileName) throws IOException, ClassNotFoundException {
         EntryManager entryManager;
 
-        ObjectInputStream in = null;
-        try {
-            in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)));
+        try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)))) {
             entryManager = (EntryManager) in.readObject();
-        } finally {
-            in.close();
         }
 
         return entryManager;
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         EntryManager entryManager = new EntryManager();
         View view = new View("Entry Manager");
-        Controller controller = new Controller(entryManager, view, true);
+        new Controller(entryManager, view, true);
     }
 }
